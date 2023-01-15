a6b1c362778a0253926cfdbe22ec35b3d8d53b9c
Aditya Kamble <adityakamble6101@gmail.com>
2023-01-15 20:11:48 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-400">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
